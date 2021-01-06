c0b8f31d66dd1e5fa9e3e553ee898b89672aa84b
3472204455ilia <68978403+3472204455ilia@users.noreply.github.com>
2021-01-05 22:47:58 -0500

    basic covered

---
+++ listFunctions.py
@@ -0,0 +1,13 @@
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(1, "Bob")
+print(numbers)
+numbers.append(7)
+print(numbers)
+numbers.remove(4)
+print(numbers)
+numbers.clear()
+print(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+print(1 in numbers)
+print(len(numbers))
---
+++ rangeFunction.py
@@ -0,0 +1,10 @@
+for number in range(5):
+    print(number)
+
+print(" form 5 to 9")
+for number in range(5, 10):
+    print(number)
+
+print(" two '2' as a step")
+for number in range(5, 10, 2):
+    print(number)
